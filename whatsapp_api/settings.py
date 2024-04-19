0d6951ab463184c68ad04c2782ee07d8b246af1c
Felipe Garcia Suez <garciasuezfelipe@gmail.com>
2024-04-19 14:47:21 -0300

    deploy indio

--- whatsapp_api/settings.py
+++ whatsapp_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lo+8plq3ed%ey$+v3!#f*x^(0o0#lb1iib30^z42!s!vqdwkra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATICFILES_BUILD = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
